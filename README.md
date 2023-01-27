9e06b943042754ef11de8de19976265ffae8d57f
jennahopeclem <jennahopeclem@gmail.com>
2023-01-27 16:07:47 -0500

    readme file added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# ecommerce-backend
+# Ecommerce-Backend 
+
+## Description
+
+The ecommerce backend is created to help companies keep track of data by viewing, adding, editing, and deleting certain criteria for their products. 
+
+For instance, this backend editor will allow an ecommerce company with product, product category, and product tags stay organized and updated.
+
+## Installation 
+
+Seeds were implemented into the server in order to retrieve data on products, categories and tags.
+
+## Usage
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,50 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get("/", async (req, res) => {
+  //FIND ALL ASSOCIATED PRODUCTS
+  try {
+    const catData = await Category.findAll({ include: [{ model: Product }] });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", async ({ params }, res) => {
+  const category = await Category.findByPk(params.id);
+  res.json(category);
 });
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  console.log(req.body);
+  const newCat = await Category.create(req.body);
+  res.json(newCat);
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  const updatedCat = await Category.update(req.body,{
+    where: {
+      id: req.params.id,
+    }
+    
+  });
+  res.json(updatedCat);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  const deletedCat = await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.json(deletedCat);
 });
 
 module.exports = router;
